aade7ffbd53e14eb589fbdf952ab23580fbcb36a
camille <camille_chen25@hotmail.fr>
2019-03-16 18:53:01 +0100

    Update veterinaire_detail.php

--- veterinaire_detail.php
+++ veterinaire_detail.php
@@ -16,6 +16,42 @@ if($_GET['id']==1){
     </ul>';
 }
 
+if($_GET['id']==2){
+    echo '<ul class="coordonnee">
+    <li><p>Nom:</p><p class="donnee">PAGE</p></li>
+    <li><p>Prénom:</p><p class="donnee">Dona</p></li>
+    <li><p>Lieu:</p><p class="donnee">Paris</p></li>
+    <li><p>Spécialité:</p><p class="donnee"> Vétérinaire reptiliens </p></li>
+    <li><p>Téléphone:</p><p class="donnee"> 06 85 98 42 69</p></li>
+    <li><p>Adresse postale:</p><p class="donnee"> 9 rue des bermudes,
+     75000, Ste Luce Sur Loire</p></li>
+    </ul>';
+}
+
+if($_GET['id']==3){
+    echo '<ul class="coordonnee">
+    <li><p>Nom:</p><p class="donnee">OMANDA</p></li>
+    <li><p>Prénom:</p><p class="donnee">Hugo</p></li>
+    <li><p>Lieu:</p><p class="donnee">Lille</p></li>
+    <li><p>Spécialité:</p><p class="donnee"> Chirurgien équidés </p></li>
+    <li><p>Téléphone:</p><p class="donnee"> 06 63 83 55 93</p></li>
+    <li><p>Adresse postale:</p><p class="donnee"> 18 avenue des rochers,
+     59000, Lille</p></li>
+    </ul>';
+}
+
+if($_GET['id']==4){
+    echo '<ul class="coordonnee">
+    <li><p>Nom:</p><p class="donnee">LE SAUX</p></li>
+    <li><p>Prénom:</p><p class="donnee">Arnaud</p></li>
+    <li><p>Lieu:</p><p class="donnee">Lyon</p></li>
+    <li><p>Spécialité:</p><p class="donnee"> Vétérinaire rongeurs </p></li>
+    <li><p>Téléphone:</p><p class="donnee"> 06 63 83 55 93</p></li>
+    <li><p>Adresse postale:</p><p class="donnee"> 18 avenue des 3 rue de la pentades,
+    69000, Lyon</p></li>
+    </ul>';
+}
+
 ?>
 
 <?php
